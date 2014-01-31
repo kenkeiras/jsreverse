f3d7724c78938090df1f7db442bb879eed351e4b
kenkeiras <kenkeiras@gmail.com>
2014-01-31 21:33:11 +0100

    Decompilating adition (got to fix variable name assignment)

--- test/source/BasicArithmetics.java
+++ test/source/BasicArithmetics.java
@@ -2,8 +2,8 @@ class BasicArithmetics {
 
 
     public int adition(int int_1, int int_2) {
-        int int_3 = int_1 + int_2;
-        return int_3;
+        int variable_3 = int_1 + int_2;
+        return variable_3;
     }
 
 }
