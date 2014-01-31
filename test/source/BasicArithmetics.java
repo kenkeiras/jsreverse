313f300db1aebd39efab4e202519d1f60625dfa3
kenkeiras <kenkeiras@gmail.com>
2014-02-01 00:49:52 +0100

    Integer arithmetics

--- test/source/BasicArithmetics.java
+++ test/source/BasicArithmetics.java
@@ -1,8 +1,20 @@
 class BasicArithmetics {
 
 
-    public int adition(int intp_1, int intp_2) {
+    public int integer_adition(int intp_1, int intp_2) {
         return intp_1 + intp_2;
+    }
+
+    public int integer_substraction(int intp_1, int intp_2) {
+        return intp_1 - intp_2;
+    }
+
+    public int integer_multiplication(int intp_1, int intp_2) {
+        return intp_1 * intp_2;
+    }
+
+    public int integer_division(int intp_1, int intp_2) {
+        return intp_1 / intp_2;
     }
 
 }
