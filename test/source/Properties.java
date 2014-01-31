5ee35ecf01660357b4202539de01e176a95ce1d8
kenkeiras <kenkeiras@gmail.com>
2014-01-31 20:41:54 +0100

    Working property read

--- test/source/Properties.java
+++ test/source/Properties.java
@@ -1,10 +1,10 @@
 class Properties {
 
-
-    String str = "";
+    String str = "\"test\" string";
     int number = 42;
     float simple = 0.5f;
     float hard = 0.125f;
-    double d = 0.25;
+    double d = 0.25f;
     long l = 400000001;
+
 }
