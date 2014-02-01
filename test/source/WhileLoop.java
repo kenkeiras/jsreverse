1334841ab7ddf52bbf72aab3524385cf8a7b074c
kenkeiras <kenkeiras@gmail.com>
2014-02-01 14:50:27 +0100

    Failing while loop tests

---
+++ test/source/WhileLoop.java
@@ -0,0 +1,34 @@
+class WhileLoop {
+
+
+    public void while_eq(int intp_1, int intp_2) {
+        while (intp_1 == intp_2){
+            intp_1++;
+        }
+    }
+
+    public void int_lt(int intp_1, int intp_2) {
+        while (intp_1 < intp_2){
+            intp_1++;
+        }
+    }
+
+    public void int_le(int intp_1, int intp_2) {
+        while (intp_1 <= intp_2){
+            intp_1++;
+        }
+    }
+
+    public void int_gt(int intp_1, int intp_2) {
+        while (intp_1 > intp_2){
+            intp_1--;
+        }
+    }
+
+    public void int_ge(int intp_1, int intp_2) {
+        while (intp_1 >= intp_2){
+            intp_1--;
+        }
+    }
+
+}
--- test/test.flow_control.js
+++ test/test.flow_control.js
@@ -7,8 +7,8 @@ var decompilationTest = require(path.join(here, "decompilation_test"));
 suite('Flow control', function(done){
     test('if branching',
          decompilationTest("IfFlow"));
-    // test('single parameter constructor',
-    //      decompilationTest("SingleParameterConstructor"));
+    test('while loop',
+         decompilationTest("WhileLoop"));
     // test('two parameter constructor',
     //      decompilationTest("TwoParameterConstructor"));
     // test('overloaded constructors',
