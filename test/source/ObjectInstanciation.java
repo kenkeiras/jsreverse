ba8e86ac238df142a3588f3275c7bc68fee058ad
kenkeiras <kenkeiras@gmail.com>
2014-02-01 02:36:06 +0100

    (Failing) object instanciation test

---
+++ test/source/ObjectInstanciation.java
@@ -0,0 +1,12 @@
+class ObjectInstanciation {
+
+    private String sample_string_param;
+
+    public ObjectInstanciation() {
+    }
+
+    public void main(String[] stringsp_1) {
+        ObjectInstanciation objectinstanciation_0 = new ObjectInstanciation();
+    }
+
+}
