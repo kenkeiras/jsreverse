f56429f6bafb9487b4d19b2cda550e1df2ce8f7d
kenkeiras <kenkeiras@gmail.com>
2014-02-01 01:00:17 +0100

    All basic arithmetic tests, double-size constansts break

--- test/source/BasicArithmetics.java
+++ test/source/BasicArithmetics.java
@@ -17,4 +17,52 @@ class BasicArithmetics {
         return intp_1 / intp_2;
     }
 
+    public float float_adition(float floatp_1, float floatp_2) {
+        return floatp_1 + floatp_2;
+    }
+
+    public float float_substraction(float floatp_1, float floatp_2) {
+        return floatp_1 - floatp_2;
+    }
+
+    public float float_multiplication(float floatp_1, float floatp_2) {
+        return floatp_1 * floatp_2;
+    }
+
+    public float float_division(float floatp_1, float floatp_2) {
+        return floatp_1 / floatp_2;
+    }
+
+    public double double_adition(double doublep_1, double doublep_2) {
+        return doublep_1 + doublep_2;
+    }
+
+    public double double_substraction(double doublep_1, double doublep_2) {
+        return doublep_1 - doublep_2;
+    }
+
+    public double double_multiplication(double doublep_1, double doublep_2) {
+        return doublep_1 * doublep_2;
+    }
+
+    public double double_division(double doublep_1, double doublep_2) {
+        return doublep_1 / doublep_2;
+    }
+
+    public long long_adition(long longp_1, long longp_2) {
+        return longp_1 + longp_2;
+    }
+
+    public long long_substraction(long longp_1, long longp_2) {
+        return longp_1 - longp_2;
+    }
+
+    public long long_multiplication(long longp_1, long longp_2) {
+        return longp_1 * longp_2;
+    }
+
+    public long long_division(long longp_1, long longp_2) {
+        return longp_1 / longp_2;
+    }
+
 }
