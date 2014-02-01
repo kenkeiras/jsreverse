aef230c4d0c8c921c0e6d0e542e3aee33d74c167
kenkeiras <kenkeiras@gmail.com>
2014-02-01 16:15:48 +0100

    Working While test

--- test/source/WhileLoop.java
+++ test/source/WhileLoop.java
@@ -3,31 +3,31 @@ class WhileLoop {
 
     public void while_eq(int intp_1, int intp_2) {
         while (intp_1 == intp_2){
-            intp_1++;
+            ++intp_1;
         }
     }
 
-    public void int_lt(int intp_1, int intp_2) {
+    public void while_lt(int intp_1, int intp_2) {
         while (intp_1 < intp_2){
-            intp_1++;
+            ++intp_1;
         }
     }
 
-    public void int_le(int intp_1, int intp_2) {
+    public void while_le(int intp_1, int intp_2) {
         while (intp_1 <= intp_2){
-            intp_1++;
+            ++intp_1;
         }
     }
 
-    public void int_gt(int intp_1, int intp_2) {
+    public void while_gt(int intp_1, int intp_2) {
         while (intp_1 > intp_2){
-            intp_1--;
+            --intp_1;
         }
     }
 
-    public void int_ge(int intp_1, int intp_2) {
+    public void while_ge(int intp_1, int intp_2) {
         while (intp_1 >= intp_2){
-            intp_1--;
+            --intp_1;
         }
     }
 
